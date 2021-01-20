c082ca8939a25c555c438a0b65a708b0a94fbc60
19zander97 <areyno07@calpoly.edu>
2021-01-20 15:06:16 -0800

    project 1.2

--- base_convert.py
+++ base_convert.py
@@ -1,9 +1,6 @@
 
 def convert(num,b):
-    rem = num%b
-    if (rem>=10):
-        rem = chr(rem + 55)
-    if (num==0): 
-        return str()
-    else:
-        return str(convert(num//b,b)) + str(rem)
+   char = "0123456789ABCDEF"  
+   if num < b: return char[num]
+   else:
+      return convert(num//b,b) + char[num % b]
