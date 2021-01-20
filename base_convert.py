448b09c0a5bc7f257d82176cfc6298d97b873051
19zander97 <areyno07@calpoly.edu>
2021-01-19 19:03:31 -0800

    2nd times a charm

--- base_convert.py
+++ base_convert.py
@@ -3,7 +3,7 @@ def convert(num,b):
     rem = num%b
     if (rem>=10):
         rem = chr(rem + 55)
-    if (num<=1): 
-        return str(num)
+    if (num==0): 
+        return str()
     else:
         return str(convert(num//b,b)) + str(rem)
